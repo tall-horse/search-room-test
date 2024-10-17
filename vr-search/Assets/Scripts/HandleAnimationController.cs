571e87c822a6525578836ef22eed7f770a6aad81
tall-horse <dima.gamedev@pm.me>
2024-10-17 15:53:39 +0200

    model import and door improvement

---
+++ vr-search/Assets/Scripts/HandleAnimationController.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+public class HandleAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private XRGrabInteractable interactable;
+    private void Awake() {
+        animator = GetComponentInChildren<Animator>();
+        interactable = GetComponentInChildren<XRGrabInteractable>();
+    }
+     private void OnEnable()
+    {
+        //interactable.hoverEntered.AddListener();
+        interactable.selectEntered.AddListener(OnGrab);
+        interactable.selectExited.AddListener(OnRelease);
+    }
+
+    private void OnDisable()
+    {
+        interactable.selectEntered.RemoveListener(OnGrab);
+        interactable.selectExited.RemoveListener(OnRelease);
+    }
+    private void OnGrab(SelectEnterEventArgs arg0)
+    {
+        animator.SetBool("IsOpening", true);
+    }
+    private void OnRelease(SelectExitEventArgs arg0)
+    {
+        animator.SetBool("IsOpening", false);
+    }
+}
