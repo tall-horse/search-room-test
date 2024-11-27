5d9d3669992554ea0bdda9ec9e5e84b8abb2b231
tall-horse <dima.gamedev@pm.me>
2024-11-27 13:16:52 +0100

    Door handles enabled when door locked

--- vr-search/Assets/Scripts/DoorUnlocker.cs
+++ vr-search/Assets/Scripts/DoorUnlocker.cs
@@ -11,25 +11,18 @@ public class DoorUnlocker : MonoBehaviour
     private const string IsOpened = "IsOpened";
     private Animator animator;
     private XRSocketInteractor socketInteractor;
-    private List<XRGrabInteractable> doorHandles = new List<XRGrabInteractable>();
     private Rigidbody doorRigidbody;
     private void Awake()
     {
         doorRigidbody = GetComponentInParent<HingeJoint>().gameObject.GetComponent<Rigidbody>();
         animator = GetComponentInParent<Animator>();
         socketInteractor = GetComponentInParent<XRSocketInteractor>();
-        doorHandles = door.GetComponentsInChildren<XRGrabInteractable>().ToList();
     }
 
     private void Start()
     {
         doorRigidbody.isKinematic = true;
         socketInteractor.selectEntered.AddListener(UnlockDoor);
-        EnableHandles(false);
-    }
-    private void EnableHandles(bool argument)
-    {
-        doorHandles.ForEach(d => d.enabled = argument);
     }
 
     private void UnlockDoor(SelectEnterEventArgs arg0)
@@ -43,7 +36,6 @@ public class DoorUnlocker : MonoBehaviour
 
     public void OpenDoor()
     {
-        EnableHandles(true);
         doorRigidbody.isKinematic = false;
         transform.parent.gameObject.SetActive(false);
         gameObject.SetActive(false);
