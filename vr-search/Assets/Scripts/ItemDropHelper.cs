46f1fe6188c8655a5a853492a2f1e197294364d6
tall-horse <dima.gamedev@pm.me>
2025-01-20 14:04:57 +0100

    assitance added when you drop item early

---
+++ vr-search/Assets/Scripts/ItemDropHelper.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class ItemDropHelper : MonoBehaviour
+{
+    private Transform originalTransform;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    private Rigidbody itemRigidbody;
+    private void Awake()
+    {
+        originalTransform = GetComponent<Transform>();
+        itemRigidbody = GetComponent<Rigidbody>();
+        originalPosition = originalTransform.position;
+        originalRotation = originalTransform.rotation;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<DroppedItemArea>() == true)
+        {
+            transform.position = originalPosition;
+            transform.rotation = originalRotation;
+            itemRigidbody.linearVelocity = Vector3.zero;
+            itemRigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+
+}
